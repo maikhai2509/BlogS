fa8cd9b960a49059b20f3f767746b2012d82a7a5
KHAI MAI HOANG <KHAI.MH184494@sis.hust.edu.vn>
2024-03-10 02:54:26 +0700

    switch to cloud db

--- backend/src/config/database/index.js
+++ backend/src/config/database/index.js
@@ -1,7 +1,10 @@
+// => connect to db local
+// ${process.env.MONGODB_SERVER}://${process.env.MONGODB_HOST}:${process.env.MONGODN_PORT}/${process.env.MONGODDB_COLLECTION}
+
 import mongoose from "mongoose"
 async function connect() {
     try {
-        const connection = await mongoose.connect(`${process.env.MONGODB_SERVER}://${process.env.MONGODB_HOST}:${process.env.MONGODN_PORT}/${process.env.MONGODDB_COLLECTION}`, {
+        const connection = await mongoose.connect(`mongodb+srv://hoangkhai8051:UitcnNSDeYfEzVgj@cluster0.gvfotah.mongodb.net/BlogS?retryWrites=true&w=majority&appName=Cluster0}`, {
             useNewUrlParser: true, 
             useUnifiedTopology: true
         })
